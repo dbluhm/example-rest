194851b39e32c7fc2355ca8a598f84ad365730d6
Daniel Bluhm <dbluhm@pm.me>
2021-09-20 18:21:46 -0400

    feat: add color to DCO
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- .github/workflows/dco_check.sh
+++ .github/workflows/dco_check.sh
@@ -4,9 +4,9 @@ revs=$(git rev-list "HEAD") || exit 1
 failed=0
 for commit in $revs; do
     if git show -s --format=%B "${commit}" | grep >/dev/null 2>&1 "Signed-off-by: "; then
-        echo -e "\u2714 $(git show -s --format='%h %s' $commit)"
+        echo -e "\e[32m\u2714\e[0m $(git show -s --format='%h %s' $commit)"
     else
-        echo -e "\u2718 $(git show -s --format='%h %s' $commit)"
+        echo -e "\e[31m\u2718 $(git show -s --format='%h %s' $commit)\e[0m"
         failed=1
     fi
 done
