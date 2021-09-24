b0b3a3d3090686324d7a67e8bd359dacb283e02f
Micah Peltier <micah6_8@yahoo.com>
2021-09-24 11:46:53 -0600

    feat: added a GET /delete/{index} to remove items from list
    Signed-off-by: Micah Peltier <micah6_8@yahoo.com>

--- example_rest/__main__.py
+++ example_rest/__main__.py
@@ -1,37 +1,47 @@
 # Here are our imports
 import fastapi
 
+app = fastapi.FastAPI()
 
-def main():
-    app = fastapi.FastAPI()
+# def main():
 
-    quotes = [
-        "You were the chosen one!",
-        "It was said that you would destroy the Sith, not join them!",
-        "Bring balance to the force, not leave it in darkness!",
-        "I HATE YOU!!",
-        "You were my brother Anakin!",
-        "I loved you!",
-    ]
-
-    # Give us something at the root
-    @app.get("/")
-    async def root():
-        return {"message": "Hello There!"}
-
-    # Return the full list
-    @app.get("/list/")
-    async def read_list():
-        return quotes
-
-    # Return a list item by index
-    @app.get("/list/{item_id}")
-    async def read_item(item_id: int):
-        if item_id >= len(quotes) or item_id < 0:
-            return {"Sorry, that item doesn't exist!"}
-
-        return {"Item " + str(item_id): quotes[item_id]}
+quotes = [
+    "You were the chosen one!",
+    "It was said that you would destroy the Sith, not join them!",
+    "Bring balance to the force, not leave it in darkness!",
+    "I HATE YOU!!",
+    "You were my brother Anakin!",
+    "I loved you!",
+]
 
 
-if __name__ == "__main__":
-    main()
+# Give us something at the root
+@app.get("/")
+async def root():
+    return {"message": "Hello There!"}
+
+
+# Return the full list
+@app.get("/list/")
+async def read_list():
+    return quotes
+
+
+# Return a list item by index
+@app.get("/list/{item_id}")
+async def read_item(item_id: int):
+    if item_id >= len(quotes) or item_id < 0:
+        return {"Sorry, that item doesn't exist!"}
+
+    return {"Item " + str(item_id): quotes[item_id]}
+
+
+@app.get("/delete/{item_id}")
+async def del_item(item_id: int):
+    if quotes[item_id] is not None:
+        quotes.pop(item_id)
+    return
+
+
+# if __name__ == "__main__":
+#     main()
